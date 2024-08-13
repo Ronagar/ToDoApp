e8f922b5bf11406414f58f5e2716d983f7eca82c
Ronagar <rnginformatica@uma.es>
2024-08-13 13:44:17 +0200

    Project created

---
+++ src/main/java/com/rober/ToDoApp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.rober.ToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
